b918c9a4bfbf36f71ad6dfed77e1f923ed21f2f9
mateuscamargo <mateuscamargolima@gmail.com>
2024-03-13 20:44:42 -0300

    edit ConnectionFactory.java

--- GEPETO_EVENTOS/src/Factory/ConnectionFactory.java
+++ GEPETO_EVENTOS/src/Factory/ConnectionFactory.java
@@ -1,16 +1,32 @@
 package Factory;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConnectionFactory {
 
-	public Connection getConnection() throws ClassNotFoundException {
+	public static void main(String[] args) throws SQLException {
+		
+		Connection conexao = null;
+		
 		try {
-			Class.forName("com.mysql.jdbc.Driver"); 
-			return DriverManager.getConnection("jdbc:mysql://localhost:3306/gepetojava","root","root@2024");
-		}         
-			catch(SQLException excecao) {
-				throw new RuntimeException(excecao);
-			 }
-	    }
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		conexao = DriverManager.getConnection("jdbc:mysql://localhost/gepeto", "root", "root@2024");
+		
+		
+		} catch (ClassNotFoundException ex) {
+			System.out.println("Driver do banco de dados não encontrado");
+		} catch (SQLException ex) {
+			System.out.println("Ocorreu um erro ao acessar o banco: " + ex.getMessage());
+		} finally {
+			if (conexao != null) {
+				conexao.close();
+			}
+		}
+		
+		
+	}
+	
 }
