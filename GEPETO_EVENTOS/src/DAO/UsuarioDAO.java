45ee3e929408bf9d55b5cfa83a2369a6eb7df68f
lukescmg <lucascmg.lima@gmail.com>
2024-03-14 19:14:20 -0300

    UsuarioDAO atualizado

--- GEPETO_EVENTOS/src/DAO/UsuarioDAO.java
+++ GEPETO_EVENTOS/src/DAO/UsuarioDAO.java
@@ -1,5 +1,47 @@
 package DAO;
 
 public class UsuarioDAO {
-
+	String UsuarioEmail;
+	String UsuarioNome;
+	String UsuarioSobrenome;
+	String UsuarioDataNascimento;
+	String UsuarioTelefone;
+	String UsuarioSenha;
+	
+	public String getUsuarioEmail() {
+		return UsuarioEmail;
+	}
+	public void setUsuarioEmail(String UsuarioEmail) {
+		this.UsuarioEmail = UsuarioEmail;
+	}
+	public String getUsuarioNome() {
+		return UsuarioNome;
+	}
+	public void setUsuarioNome(String UsuarioNome) {
+		this.UsuarioNome = UsuarioNome;
+	}
+	public String getUsuarioSobrenome() {
+		return UsuarioSobrenome;
+	}
+	public void setUsuarioSobrenome(String UsuarioSobrenome) {
+		this.UsuarioSobrenome = UsuarioSobrenome;
+	}
+	public String getUsuarioDataNascimento() {
+		return UsuarioDataNascimento;
+	}
+	public void setUsuarioDataNascimento(String UsuarioDataNascimento) {
+		this.UsuarioDataNascimento = UsuarioDataNascimento;
+	}
+	public String getUsuarioTelefone() {
+		return UsuarioTelefone;
+	}
+	public void setUsuarioTelefone(String UsuarioTelefone) {
+		this.UsuarioTelefone = UsuarioTelefone;
+	}
+	public String getUsuarioSenha() {
+		return UsuarioSenha;
+	}
+	public void setUsuarioSenha(String UsuarioSenha) {
+		this.UsuarioSenha = UsuarioSenha;
+	}
 }
